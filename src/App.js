8f4380e8c921757a22191e6e8de25a12fc5f6ec7
sehannp <sehanp@gmail.com>
2020-02-22 19:57:57 +1100

    finished quote feature rest API call

--- src/App.js
+++ src/App.js
@@ -13,12 +13,30 @@ class App extends Component {
     super();
     this.state = {
       activities: [],
-      totalHours: 0
+      totalHours: 0,
+      quote: ''
     }
     this.onSubmit = this.onSubmit.bind(this);
     this.onTotalHours = this.onTotalHours.bind(this);
   }
 
+  componentDidMount(){
+    fetch("https://type.fit/api/quotes")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      const basedate = new Date();
+      const datecount = (Date.UTC(basedate.getFullYear(), basedate.getMonth(), basedate.getDate()) 
+      - Date.UTC(basedate.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
+      return (data[datecount]);
+    })
+    .then(data => {
+      this.setState({quote:'"'+data.text+'" - '+data.author});
+    })
+      
+  }
+  
   onSubmit(activity,startime, endtime){
     this.setState({
       activities: [
@@ -36,6 +54,11 @@ class App extends Component {
     return (
       <div className="App">
         <Header></Header>
+        <hr/>
+        <div className="clocks">
+          <h1>Quote for the Day</h1>
+            <blockquote cite="https://type.fit/api/quotes">{this.state.quote}</blockquote>
+        </div>
         <hr/>
         <div className="clocks">
           <h1>Clocks</h1>
