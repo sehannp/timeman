b7792dc6b5ec66298a79eef449ffa65f9ad1e020
sehannp <sehanp@gmail.com>
2020-02-24 19:09:48 +1100

    persistance added (redux-persist)

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,21 @@
 import {combineReducers} from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import activityReducer from './activity/activityReducer';
 import appReducer from './app/appReducer';
 
-export default combineReducers({
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['activity']
+  };
+
+  
+const rootReducer = combineReducers({
     activity: activityReducer,
     app: appReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import {createStore} from 'redux';
+import { persistStore } from 'redux-persist';
+
 import rootReducer from './rootReducer';
 
 import logger from 'redux-logger';
 import { applyMiddleware } from 'redux';
 const middlewares = [logger]
 
-const store = createStore(rootReducer,applyMiddleware(...middlewares));
-
-export default store;
+export const store = createStore(rootReducer,applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
+export default { store, persistStore };
